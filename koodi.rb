dee573c577a6d90bd27c76399ac1e611ffe82ee0
Satu Häkkinen <s.j.hakkinen@gmail.com>
2016-01-21 21:12:05 +0200

    Hashiin männöö nyt mitä pitääkin

--- koodi.rb
+++ koodi.rb
@@ -1,1 +1,7 @@
-# kirjoita koodi tänne
+def esiintymat(taulukko)
+  hash = {}
+  0.upto((taulukko.length-1)){ |i| hash[taulukko.at(i)] = taulukko.count(taulukko.at(i)) }
+  hash
+end
+
+esiintymat([1,5,5,5,10])
